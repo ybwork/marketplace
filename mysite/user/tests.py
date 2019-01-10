4b36b78383d7ffbfe0ece6660931a43c81b5227f
Ilya Kaduk <kaduk2593@gmail.com>
2019-01-10 13:44:33 +0300

    Добавление расчетных счетов refs #17570
    - переопределил вывод (каждый видит только свои счета)
    - переопределил создание (user_id подставляется автоматом и текущего пользователя)

--- mysite/user/tests.py
+++ mysite/user/tests.py
@@ -1,15 +1,1 @@
-from django.db.models import QuerySet
 from django.test import TestCase
-
-# создание расчётного счёта
-
-# from user.models import Invoice
-#
-# from user.admin import InvoiceAdmin
-#
-#
-# class InvoiceAdminTestCase(TestCase):
-#     def test_create_invoice(self):
-#         invoice = InvoiceAdmin()
-#         invoice.save_model()
-#         self.assertEqual(Invoice(user_id=4, num='lll333'), object)
