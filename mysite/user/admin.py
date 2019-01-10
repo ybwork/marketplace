7105bc370d9059010afc776696694ac27be9effd
Ilya Kaduk <kaduk2593@gmail.com>
2019-01-10 13:22:13 +0300

    Сделал вывод расчётных счетов (каждый видит только свои)

--- mysite/user/admin.py
+++ mysite/user/admin.py
@@ -6,6 +6,9 @@ from user.models import Invoice
 class InvoiceAdmin(admin.ModelAdmin):
     fields = ('num',)
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(user=request.user.pk)
+
     def save_model(self, request, obj, form, change):
         obj.user_id = request.user.pk
         obj.save()
--- mysite/user/tests.py
+++ mysite/user/tests.py
@@ -1,3 +1,15 @@
+from django.db.models import QuerySet
 from django.test import TestCase
 
-# Create your tests here.
+# создание расчётного счёта
+
+# from user.models import Invoice
+#
+# from user.admin import InvoiceAdmin
+#
+#
+# class InvoiceAdminTestCase(TestCase):
+#     def test_create_invoice(self):
+#         invoice = InvoiceAdmin()
+#         invoice.save_model()
+#         self.assertEqual(Invoice(user_id=4, num='lll333'), object)
