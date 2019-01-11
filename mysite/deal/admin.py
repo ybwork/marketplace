6aae29542aff66376e8ec868f204064981c22f0b
Ilya Kaduk <kaduk2593@gmail.com>
2019-01-11 12:33:17 +0300

    Убрал кнопку добавить, если есть хоть одна запись

--- mysite/deal/admin.py
+++ mysite/deal/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from deal.models import Status, Commission
 
+
+class CommissionAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        if Commission.objects.exists():
+            return False
+        return True
+
+
 admin.site.register(Status)
-admin.site.register(Commission)
+admin.site.register(Commission, CommissionAdmin)
