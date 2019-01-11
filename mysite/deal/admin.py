47dcd3cd58a19351bc51841311e80e3000a3b19b
Ilya Kaduk <kaduk2593@gmail.com>
2019-01-11 10:32:37 +0300

    Создание предложения refs #17612
    - изменил вывод (каждый пользователь может видеть только свои)
    - изменил создание (автоматом подставляется id пользователя, который создал)
    - убрал вывод поля user в админке

--- mysite/deal/admin.py
+++ mysite/deal/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from deal.models import Status, Commission
+from deal.models import Status, Commission, Offer
+
+
+class OfferAdmin(admin.ModelAdmin):
+    exclude = ('user',)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(user=request.user)
+
 
 admin.site.register(Status)
 admin.site.register(Commission)
+admin.site.register(Offer, OfferAdmin)
