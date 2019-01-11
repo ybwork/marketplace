c397fd9f7ffb9f582912341ddf30d779d7cbd51e
Ilya Kaduk <kaduk2593@gmail.com>
2019-01-11 10:42:12 +0300

    Небольшие изменения refs #17612
    - исправил создание
    - привел код админской модели к общему виду

--- mysite/user/admin.py
+++ mysite/user/admin.py
@@ -6,12 +6,12 @@ from user.models import Invoice
 class InvoiceAdmin(admin.ModelAdmin):
     fields = ('num',)
 
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
     def get_queryset(self, request):
         return super().get_queryset(request).filter(user=request.user.pk)
-
-    def save_model(self, request, obj, form, change):
-        obj.user_id = request.user.pk
-        obj.save()
 
 
 admin.site.register(Invoice, InvoiceAdmin)
