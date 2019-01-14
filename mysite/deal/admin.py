2feb092c6296e742b68453e3c3376fe3213ebea6
Ilya Kaduk <kaduk2593@gmail.com>
2019-01-14 10:46:34 +0300

    Добавил ограничения для вывода всех предложений

--- mysite/deal/admin.py
+++ mysite/deal/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from deal.models import Status, Commission, Offer
+from deal.models import Status, Commission, Offer, NewOffer
 
 
 class OfferAdmin(admin.ModelAdmin):
@@ -12,6 +12,17 @@ class OfferAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         return super().get_queryset(request).filter(user=request.user)
+
+
+class AllOffersAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class CommissionAdmin(admin.ModelAdmin):
