b4a44d69ad62042b7197c26417595bbc0ddf5312
Ilya Kaduk <kaduk2593@gmail.com>
2019-01-31 10:38:58 +0300

    Новая система работы с api v-1

--- mysite/deal/utils.py
+++ mysite/deal/utils.py
@@ -10,92 +10,7 @@ from mysite.settings import API_KEY
 from deal.exceptions import InternalServerError, UnauthorizedError, \
     NotFoundError, OtherStatusCodes, NotEnoughMoney, BadRequestError
 
-
-def get_balance_user(invoice):
-    req = requests.get(
-        'http://127.0.0.1:5000/v1/invoices/{invoice}/balances'.format(
-            invoice=invoice
-        ),
-        json={
-            'api_key': API_KEY,
-        }
-    )
-    check_status_code(req.status_code)
-    return decimal.Decimal(
-        json.loads(req.content)['balance']
-    )
-
-
-def check_status_code(status_code):
-    if status_code == 200:
-        return True
-
-    if status_code == 400:
-        raise BadRequestError('Отсутствуют параметры запроса')
-
-    if status_code == 401:
-        raise UnauthorizedError('Не авторизован')
-
-    if status_code == 404:
-        raise NotFoundError('Объект не найден')
-
-    if status_code == 500:
-        raise InternalServerError('Что то пошло не так, попробуйте позже')
-
-    raise OtherStatusCodes()
-
-
-def check_user_balance(invoice, amount_money_payment):
-    balance = get_balance_user(invoice)
-    if balance < amount_money_payment:
-        raise NotEnoughMoney('Не хватает денег на счете')
-
-
-def pay(amount_money, number_invoice_provider, number_invoice_reciever):
-    req = requests.post(
-        'http://127.0.0.1:5000/v1/payments',
-        json={
-            'api_key': API_KEY,
-            'amount_money': str(amount_money),
-            'number_invoice_provider': number_invoice_provider,
-            'number_invoice_reciever': number_invoice_reciever
-        }
-    )
-    return check_status_code(req.status_code)
-
-
-def confirm_payment(invoice, code_confirm):
-    req = requests.post(
-        'http://127.0.0.1:5000/v1/payments/confirm',
-        json={
-            'api_key': API_KEY,
-            'invoice': invoice,
-            'code_confirm': code_confirm
-        }
-    )
-    check_status_code(req.status_code)
-    return json.loads(req.content)['key']
-
-
-def perform_payment(key):
-    req = requests.post(
-        'http://127.0.0.1:5000/v1/payments/perform',
-        json={
-            'api_key': API_KEY,
-            'key': key
-        }
-    )
-    return check_status_code(req.status_code)
-
-
-def available_request_methods(http_methods=[]):
-    def decorator(function_to_decorate):
-        def original(self, request, *args, **kwargs):
-            if request.method not in http_methods:
-                return redirect(request.META['HTTP_REFERER'])
-            return function_to_decorate(self, request, *args, **kwargs)
-        return original
-    return decorator
+from mysite.settings import API_DOMEN
 
 
 def handle_api_response(function_to_decorate):
@@ -124,3 +39,100 @@ def handle_api_response(function_to_decorate):
                 template_name='errors/500.html'
             )
     return original
+
+
+def get_balance_user(invoice):
+    response = send_request(
+        url='/v1/invoices/{invoice}/balances'.format(invoice=invoice),
+        method='get',
+        params={
+            'api_key': API_KEY,
+        }
+    )
+    return decimal.Decimal(response['balance'])
+
+
+def send_request(url, method, params):
+    req = getattr(requests, method)(
+        '{domen}{url}'.format(domen=API_DOMEN, url=url),
+        json=params
+    )
+    check_status_code(status_code=req.status_code)
+    return parse_response(response=req.content)
+
+
+def check_status_code(status_code):
+    if status_code == 200:
+        return True
+
+    if status_code == 400:
+        raise BadRequestError('Отсутствуют параметры запроса')
+
+    if status_code == 401:
+        raise UnauthorizedError('Не авторизован')
+
+    if status_code == 404:
+        raise NotFoundError('Объект не найден')
+
+    if status_code == 500:
+        raise InternalServerError('Что то пошло не так, попробуйте позже')
+
+    raise OtherStatusCodes()
+
+
+def parse_response(response):
+    return json.loads(response)
+
+
+def check_user_balance(invoice, amount_money_payment):
+    balance = get_balance_user(invoice)
+    if balance < amount_money_payment:
+        raise NotEnoughMoney('Не хватает денег на счете')
+
+
+def pay(amount_money, number_invoice_provider, number_invoice_reciever):
+    return send_request(
+        url='/v1/payments',
+        method='post',
+        params={
+            'api_key': API_KEY,
+            'amount_money': str(amount_money),
+            'number_invoice_provider': number_invoice_provider,
+            'number_invoice_reciever': number_invoice_reciever
+        }
+    )
+
+
+def confirm_payment(invoice, code_confirm):
+    response = send_request(
+        url='/v1/payments/confirm',
+        method='post',
+        params={
+            'api_key': API_KEY,
+            'invoice': invoice,
+            'code_confirm': code_confirm
+        }
+    )
+    return response['key']
+
+
+def perform_payment(key):
+    return send_request(
+        url='/v1/payments/perform',
+        method='post',
+        params={
+            'api_key': API_KEY,
+            'key': key
+        }
+    )
+
+
+def available_request_methods(http_methods=[]):
+    def decorator(function_to_decorate):
+        def original(self, request, *args, **kwargs):
+            if request.method not in http_methods:
+                return redirect(request.META['HTTP_REFERER'])
+            return function_to_decorate(self, request, *args, **kwargs)
+        return original
+    return decorator
+
