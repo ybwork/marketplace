f3a2da1f72cd659a384a6f6a6d2a158b938fa9c6
Ilya <kaduk2593@gmail.com>
2019-02-12 17:20:29 +0300

    Небольшие исправления refs #17830
    - исправил миграции

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(16ui*llf%o4drunhkf$yc@rygmyv$rpk=8s*rp15evp1df*ag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
