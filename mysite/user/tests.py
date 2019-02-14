983d83477ac3d736ed99205a7f12ef205f99ee4e
Ilya <kaduk2593@gmail.com>
2019-02-14 10:41:50 +0300

    Добавил тест, который проверяет успешность создания счета с привязкой к нему текущего пользователя

--- mysite/user/tests.py
+++ mysite/user/tests.py
@@ -1,15 +1,14 @@
 from django.contrib.admin import AdminSite
 from django.contrib.auth.models import User
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
-
-from user.admin import InvoiceAdmin
 
 from user.models import Invoice
 
 
 class InvoiceAdminTests(TestCase):
     def setUp(self):
+        self.factory = RequestFactory()
         User.objects.create_user(
             'masha',
             'masha@gmail.com',
@@ -19,12 +18,11 @@ class InvoiceAdminTests(TestCase):
         )
         self.client = Client()
         self.client.login(username='masha', password='asdf1234*')
-    #
-    # def test_index_view_with_no_invoices(self):
-    #     response = self.client.get(reverse('admin:user_invoice_changelist'))
-    #     self.assertEqual(response.status_code, 200)
 
-    def test_save_model(self):
-        model_admin = InvoiceAdmin(model=Invoice, admin_site=AdminSite())
-        result = model_admin.save_model(request=None, obj=Invoice(user=User.objects.first(), num='dfdfdf'), form=None, change=None)
-        print(result)
+    def test_index_view_with_no_invoices(self):
+        response = self.client.get(reverse('admin:user_invoice_changelist'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_save_invoice_with_current_user(self):
+        response = self.client.post(reverse('admin:user_invoice_add'), {'num': 'x23md'})
+        self.assertEqual(Invoice.objects.last().user, response.wsgi_request.user)
