d90a80a06379ebf294fe65fd5032a188ba2e2d13
Ilya <kaduk2593@gmail.com>
2019-02-15 17:17:49 +0300

    Тесты для счета пользователя
    - почистил код
    - добавил тест, который проверяет, что пользователь видит только свои счета

--- mysite/user/tests.py
+++ mysite/user/tests.py
@@ -8,41 +8,95 @@ from user.models import Invoice
 
 
 class InvoiceAdminTests(TestCase):
+    user = {
+        'admin': {
+            'name': 'admin',
+            'email': 'admin@gmail.com',
+            'password': 'asdf1234!'
+        },
+        'user1': {
+            'name': 'mike',
+            'email': 'mike@gmail.com',
+            'password': 'asdf1234@'
+        },
+        'user2': {
+            'name': 'vika',
+            'email': 'vika@gmail.com',
+            'password': 'asdf1234#'
+        }
+    }
+
+    def get_user(self, name):
+        return self.user[name]
+
     def setUp(self):
+        admin = self.get_user('admin')
         User.objects.create_user(
-            username='admin',
-            email='admin@gmail.com',
-            password='asdf1234@',
+            username=admin['name'],
+            email=admin['email'],
+            password=admin['password'],
             is_staff=True,
             is_superuser=True
         )
-
-        self.user = user = User.objects.create_user(
-            username='mike',
-            email='mike@gmail.com',
-            password='asdf1234~',
+        user1 = self.get_user('user1')
+        user1_obj = User.objects.create_user(
+            username=user1['name'],
+            email=user1['email'],
+            password=user1['password'],
             is_staff=True,
         )
-        permission = Permission.objects.filter(
+        user2 = self.get_user('user2')
+        user2_obj = User.objects.create_user(
+            username=user2['name'],
+            email=user2['email'],
+            password=user2['password'],
+            is_staff=True,
+        )
+
+        permissions = Permission.objects.filter(
             content_type=ContentType.objects.get_for_model(Invoice)
         ).filter(codename__in=['add_invoice', 'view_invoice'])
-        user.user_permissions.set(permission)
+        user1_obj.user_permissions.set(permissions)
+        user2_obj.user_permissions.set(permissions)
 
+        self.user1_obj = user1_obj
+        self.user2_obj = user2_obj
         self.client = Client()
 
     def test_save_invoice_with_current_user(self):
-        self.client.login(username='mike', password='asdf1234~')
+        user = self.get_user('user1')
+        self.client.login(username=user['name'], password=user['password'])
         response = self.client.post(reverse('admin:user_invoice_add'), {'num': 'x23md'})
         self.assertEqual(Invoice.objects.last().user, response.wsgi_request.user)
 
     def test_superuser_sees_all_invoices(self):
-        self.client.login(username='mike', password='asdf1234~')
+        user1 = self.get_user('user1')
+        self.client.login(username=user1['name'], password=user1['password'])
         Invoice.objects.bulk_create([
-            Invoice(num='aaa', user=self.user),
-            Invoice(num='bbb', user=self.user)
+            Invoice(num='aaa', user=self.user1_obj),
+            Invoice(num='bbb', user=self.user1_obj)
         ])
 
-        self.client.login(username='admin', password='asdf1234@')
+        admin = self.get_user('admin')
+        self.client.login(username=admin['name'], password=admin['password'])
         response = self.client.get(reverse('admin:user_invoice_changelist'))
         invoice_list = response.context['cl'].queryset
         self.assertQuerysetEqual(invoice_list, ['<Invoice: bbb>', '<Invoice: aaa>'])
+
+    def test_user_sees_only_his_invoice(self):
+        user1 = self.get_user('user1')
+        self.client.login(username=user1['name'], password=user1['password'])
+        Invoice.objects.bulk_create([
+            Invoice(num='aaa', user=self.user1_obj),
+        ])
+        self.client.logout()
+
+        user2 = self.get_user('user2')
+        self.client.login(username=user2['name'], password=user2['password'])
+        Invoice.objects.bulk_create([
+            Invoice(num='bbb', user=self.user2_obj)
+        ])
+
+        response = self.client.get(reverse('admin:user_invoice_changelist'))
+        invoice_list = response.context['cl'].queryset
+        self.assertQuerysetEqual(invoice_list, ['<Invoice: bbb>'])
