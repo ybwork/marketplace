6a8747f42f818b25cba07ee021aae305ec7349bb
Ilya <kaduk2593@gmail.com>
2019-02-25 11:14:39 +0300

    Настроил celery

--- mysite/user/admin.py
+++ mysite/user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from user.models import Invoice
+from deal.tasks import add
 
 
 class InvoiceAdmin(admin.ModelAdmin):
@@ -11,6 +12,7 @@ class InvoiceAdmin(admin.ModelAdmin):
         obj.save()
 
     def get_queryset(self, request):
+        add.delay(1, 2)
         if request.user.is_superuser:
             return super().get_queryset(request).filter()
         return super().get_queryset(request).filter(user=request.user.pk)
