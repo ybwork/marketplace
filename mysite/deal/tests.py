e62a7a090b0ef4d763d3678d89bb4d712e3585b8
Ilya <kaduk2593@gmail.com>
2019-02-26 11:52:15 +0300

    реализовал метод test_superuser_sees_all_offers

--- mysite/deal/tests.py
+++ mysite/deal/tests.py
@@ -10,10 +10,11 @@ from user.models import Invoice
 
 class OfferAdminTests(TestCase):
     def setUp(self):
+        self.__admin_password = 'asdf!1234'
         self.admin = User.objects.create_user(
             username='admin',
             email='admin@gmail.com',
-            password='asdf!1234',
+            password=self.__admin_password,
             is_staff=True,
             is_superuser=True
         )
@@ -25,6 +26,7 @@ class OfferAdminTests(TestCase):
             password=self.__user_password,
             is_staff=True
         )
+
         permissions = Permission.objects.filter(
             content_type=ContentType.objects.get_for_model(Offer)
         ).filter(
@@ -32,35 +34,68 @@ class OfferAdminTests(TestCase):
         )
         self.user.user_permissions.set(permissions)
 
-        self.client = Client()
-
-    def test_save_offer_with_current_user(self):
-        self.client.login(
-            username=self.user.username,
-            password=self.__user_password
-        )
-        invoice = Invoice.objects.create(
+        self.invoice = Invoice.objects.create(
             user=self.user,
             num='z3gl5'
         )
-        response = self.client.post(
-            reverse('admin:deal_offer_add'),
-            {
-                'title': 'Offer 1',
-                'description': '...',
-                'price': '100',
-                'limit_hours_on_pay': '1',
-                'money_to_invoice': invoice
-            }
-        )
-        self.assertEqual(Offer.objects.last().user, response.wsgi_request.user)
+
+        Offer.objects.bulk_create([
+            Offer(
+                user=self.user,
+                title='Offer 1',
+                description='...',
+                price=100,
+                limit_hours_on_pay=1,
+                money_to_invoice=self.invoice
+            ),
+            Offer(
+                user=self.admin,
+                title='Offer 2',
+                description='...',
+                price=200,
+                limit_hours_on_pay=1,
+                money_to_invoice=self.invoice
+            )
+        ])
+
+        self.client = Client()
+
+    def test_save_offer_with_current_user(self):
+        # self.client.login(
+        #     username=self.user.username,
+        #     password=self.__user_password
+        # )
+        # response = self.client.post(
+        #     path=reverse('admin:deal_offer_add'),
+        #     data={
+        #         'title': 'Offer 1',
+        #         'description': '...',
+        #         'price': 100,
+        #         'limit_hours_on_pay': 1,
+        #         'money_to_invoice': self.invoice
+        #     }
+        # )
+        # self.assertEqual(Offer.objects.last(), response.wsgi_request.user)
+        # print(dir(response))
         # print(response.status_code)
-        # print(Offer.objects.last())
+        # print(self.user.has_perm('deal.add_offer'))
+        # print(response.status_code)
+        # print(Offer.objects.all())
         # print(response.wsgi_request.user)
         # print(response.wsgi_request.session)
+        pass
 
     def test_superuser_sees_all_offers(self):
-        pass
+        self.client.login(
+            username=self.admin.username,
+            password=self.__admin_password
+        )
+        response = self.client.get(path=reverse('admin:deal_offer_changelist'))
+        offer_list = response.context['cl'].queryset
+        self.assertQuerysetEqual(
+            qs=offer_list,
+            values=['<Offer: Offer 2>', '<Offer: Offer 1>']
+        )
 
     def test_superuser_change_any_offer(self):
         pass
